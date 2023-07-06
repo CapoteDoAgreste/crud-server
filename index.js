7a7c2d57927aeedf1eac6adb2f76ce00a604483f
CapoteDoAgreste <highhard2012@gmail.com>
2023-07-06 17:31:53 -0300

    bug fixes

--- index.js
+++ index.js
@@ -10,16 +10,6 @@ const db = mysql.createPool({
   database: "crudgames",
 });
 
-/*
-app.get("/", (req, res) => {
-  let SQL_Script =
-    "insert into games (name,cost, category) values ('Far Cry 5', '120','Ação')";
-
-  db.query(SQL_Script, (err, result) => {
-    console.log(err);
-  });
-});
-*/
 app.use(cors());
 app.use(express.json());
 
@@ -32,12 +22,47 @@ app.get("/getCards", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  const { name } = req.body;
-  const { cost } = req.body;
-  const { category } = req.body;
+  const { name, cost, category } = req.body;
 
-  let SQL_Script = `insert into games (name,cost, category) values ('${name}', '${cost}','${category}')`;
-  db.query(SQL_Script, (err, result) => {
+  if (name != undefined && cost != undefined && category != undefined) {
+    let SQL_Script = `insert into games (name,cost, category) values ('${name}', round('${cost}',2),'${category}')`;
+    console.log("creating");
+    db.query(SQL_Script, (err, result) => {
+      if (err) console.log(err);
+      else res.send(result);
+    });
+  } else {
+    res.status(400).send("Invalid Game");
+  }
+});
+
+app.put("/edit", (req, res) => {
+  const { id, name, cost, category } = req.body;
+
+  condition2 = name != "" && cost != "" && category != "";
+
+  if (
+    name != undefined &&
+    cost != undefined &&
+    category != undefined &&
+    condition2
+  ) {
+    console.log(
+      `Updated ${id} game with name:${name}, cost:${cost} and ${category}`
+    );
+    let SQL_Script = `update crudgames.games set name = '${name}', cost = round('${cost}',2), category = '${category}' where idgames = ${id}`;
+    db.query(SQL_Script, [name, cost, category, id], (err, result) => {
+      if (err) console.log(err);
+      else res.send(result);
+    });
+  }
+});
+
+app.delete("/delete/:id", (req, res) => {
+  const { id } = req.params;
+
+  let SQL_Script = `Delete from crudgames.games where idgames = ${id}`;
+  db.query(SQL_Script, [id], (err, result) => {
     if (err) console.log(err);
     else res.send(result);
   });
